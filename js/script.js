1293400b623a215714e4a70d1dd6dcff109e20e0
Angel Garcia <angeliux.jossue@gmail.com>
2022-10-20 22:03:11 -0500

    fixed problems

--- js/script.js
+++ js/script.js
@@ -8,9 +8,30 @@ menu.onclick = () => {
 }
 
 // Delete selection of menu
-menu.onscroll = () => {
-    menu.classList.remove("fa-times");
-    navbar.classList.remove("active");
+// Scrollspy 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+window.onscroll = () => {
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
 }
 
 // Interactivity of form search 
@@ -59,13 +80,23 @@ var swiper = new Swiper(".review-slider", {
         },
         768: {
             slidesPerView: 2,
-        }, 
+        },
         1024: {
             slidesPerView: 3,
         },
-    },   
+    },
 });
 
+// loader
+function loader(){
+    document.querySelector(".loader__container").classList.add("fade__out");
+}
+
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
 
 
 
